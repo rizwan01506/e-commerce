6faf8db3ad0ca6640321b2ce346014c193e6999d
Rizwan <rizwan01506@gmail.com>
2023-04-13 23:57:37 +0530

    Change logo for project

--- components/common/PageSEO.jsx
+++ components/common/PageSEO.jsx
@@ -2,24 +2,24 @@ import Head from "next/head";
 import React from "react";
 
 const PageSEO = ({ title }) => {
-  const maintitle = `Norlon — ${title}`;
+  const maintitle = `24X7 — ${title}`;
   return (
     <>
       {title && (
         <Head>
           <title>{maintitle}</title>
-          <meta name="title" content={`Norlon — ${title}`} />
+          <meta name="title" content={`24X7 — ${title}`} />
           <meta name="description" content="." />
 
           <meta property="og:type" content="website" />
           <meta property="og:url" content="" />
-          <meta property="og:title" content={`Norlon — ${title}`} />
+          <meta property="og:title" content={`24X7 — ${title}`} />
           <meta property="og:description" content="." />
           <meta property="og:image" content="" />
 
           <meta property="twitter:card" content="" />
           <meta property="twitter:url" content="" />
-          <meta property="twitter:title" content={`Norlon — ${title}`} />
+          <meta property="twitter:title" content={`24X7 — ${title}`} />
           <meta property="twitter:description" content="" />
           <meta property="twitter:image" content="" />
         </Head>
